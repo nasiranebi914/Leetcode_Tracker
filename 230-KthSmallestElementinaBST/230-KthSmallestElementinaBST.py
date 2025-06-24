423b2bfa4e0fceaf29df556da072f2a7ed5648cd
Nasira Nebi <72615859+nasiranebi914@users.noreply.github.com>
2025-06-24 15:10:34 -0500

    File updated at 6/24/2025, 3:10:36 PM

--- 230-KthSmallestElementinaBST/230-KthSmallestElementinaBST.py
+++ 230-KthSmallestElementinaBST/230-KthSmallestElementinaBST.py
@@ -1,4 +1,4 @@
-# Last updated: 6/24/2025, 2:48:42 PM
+# Last updated: 6/24/2025, 3:10:36 PM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -7,17 +7,18 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        self.k = k
-        self.ans = 0
-        def in_order(node):
-            if not node: return
-            
-            in_order(node.left)
-            self.k -= 1
-            if self.k == 0:
-                self.ans = node.val
-                return
-            
-            in_order(node.right)
-        in_order(root)
-        return self.ans
+        stack = []
+        
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            k -= 1
+            if k == 0:
+                return root.val
+            root = root.right
+        
+
+
+        
