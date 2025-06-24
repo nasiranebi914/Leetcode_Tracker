c5c3d6ea7b932969e18a1a24f89177def0322e86
Nasira Nebi <72615859+nasiranebi914@users.noreply.github.com>
2025-06-23 21:49:12 -0500

    File updated at 6/23/2025, 9:49:13 PM

--- 199-BinaryTreeRightSideView/199-BinaryTreeRightSideView.py
+++ 199-BinaryTreeRightSideView/199-BinaryTreeRightSideView.py
@@ -1,4 +1,4 @@
-# Last updated: 6/23/2025, 9:47:10 PM
+# Last updated: 6/23/2025, 9:49:13 PM
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,17 +6,32 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def goodNodes(self, root: TreeNode) -> int:        
-        def dfs(node, max_so_far):
+    def goodNodes(self, root: TreeNode) -> int:
+        def dfs(node, max_seen) -> int:
             if node is None:
                 return 0
-            if node.val >= max_so_far:
-                counter = 1
-            else:
-                counter = 0
-            max_so_far = max(max_so_far, node.val)
-            counter += dfs(node.left, max_so_far)
-            counter += dfs(node.right, max_so_far)
-            return counter
-                
-        return dfs(root, root.val)
+            
+            i = 0
+            if node.val >= max_seen:
+                max_seen = node.val
+                i = 1
+
+            return i + dfs(node.left, max_seen) + dfs(node.right, max_seen)
+
+        return dfs(root, float('-inf'))
+
+
+    # def goodNodes(self, root: TreeNode) -> int:
+    #     stack = [(root, float('-inf'))]
+    #     ans = 0
+    #     while stack:
+    #         node, max_seen = stack.pop()
+    #         if node:
+    #             if max_seen <= node.val:
+    #                 max_seen = node.val
+    #                 ans += 1
+
+    #             stack.append((node.left, max_seen))
+    #             stack.append((node.right, max_seen))
+
+    #     return ans
