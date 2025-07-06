4ca44c1a7c7ae578a848336ff0f313f733ec4e5a
Nasira Nebi <72615859+nasiranebi914@users.noreply.github.com>
2025-07-06 15:58:50 -0500

    File updated at 7/6/2025, 3:58:50 PM

--- 78-Subsets/78-Subsets.py
+++ 78-Subsets/78-Subsets.py
@@ -1,13 +1,14 @@
-# Last updated: 7/6/2025, 12:39:41 PM
+# Last updated: 7/6/2025, 3:58:50 PM
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        self.res = []
+        res = []
+        n = len(nums)
 
-        def backtrack(start, path):
-            self.res.append(path[:])
-            for i in range(start, len(nums)):
-                path.append(nums[i])
-                backtrack(i+1, path)
-                path.pop()
-        backtrack(0, [])
-        return self.res
+        for mask in range(1 << n):
+            subset = []
+            for i in range(n):
+                if (mask >> i) & 1:
+                    subset.append(nums[i])
+            res.append(subset)
+
+        return res
