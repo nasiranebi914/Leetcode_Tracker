c828d5b14195dec8c0ae37db04ca0b9f8bb04bd0
Nasira Nebi <72615859+nasiranebi914@users.noreply.github.com>
2025-07-15 16:35:29 -0500

    File updated at 7/15/2025, 4:35:29 PM

--- 994-RottingOranges/994-RottingOranges.py
+++ 994-RottingOranges/994-RottingOranges.py
@@ -1,30 +1,24 @@
-# Last updated: 7/15/2025, 4:09:27 PM
+# Last updated: 7/15/2025, 4:35:29 PM
 class Solution:
-    def orangesRotting(self, grid: List[List[int]]) -> int:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         queue = deque()
-        rows = len(grid)
-        cols = len(grid[0])
-        fresh = 0
+        rows = len(mat)
+        cols = len(mat[0])
 
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == 2:
+                if mat[r][c] == 0:
                     queue.append((r,c))
-                if grid[r][c] == 1:
-                    fresh += 1
+                else:
+                    mat[r][c] = -1
         
-        directions = [(1,0), (-1,0), (0,-1), (0,1)]
-        minute = 0
+        directions = [(1,0), (-1,0),(0,-1), (0,1)]
 
-        while queue and fresh > 0:
-            for i in range(len(queue)):
-                r,c = queue.popleft()
-                for dr,dc in directions:
-                    nr,nc = dr+r, dc+c
-                    if 0<=nr<rows and 0<=nc<cols and grid[nr][nc] == 1:
-                        grid[nr][nc] = 2
-                        queue.append((nr,nc))
-                        fresh -= 1
-            minute += 1
-        
-        return minute if fresh is 0 else -1
+        while queue:
+            r,c = queue.popleft()
+            for dr,dc in directions:
+                nr,nc = dr+r,dc+c
+                if 0<=nr<rows and 0<=nc<cols and mat[nr][nc] == -1:
+                    mat[nr][nc] = mat[r][c]+1
+                    queue.append((nr,nc))
+        return mat
