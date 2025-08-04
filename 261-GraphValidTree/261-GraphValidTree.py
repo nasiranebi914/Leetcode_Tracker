e572f3ae62321d7ea3256472eeff3b6ab6ed2b21
Nasira Nebi <72615859+nasiranebi914@users.noreply.github.com>
2025-08-04 17:50:15 -0500

    File updated at 8/4/2025, 5:50:15 PM

--- 261-GraphValidTree/261-GraphValidTree.py
+++ 261-GraphValidTree/261-GraphValidTree.py
@@ -1,22 +1,37 @@
-# Last updated: 7/23/2025, 10:29:38 AM
+# Last updated: 8/4/2025, 5:50:15 PM
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        # A tree is valid if there is no cycle, and all nodes are connected
+        # to detect if all nodes are connected, we use below
         if len(edges) != n-1:
             return False
-        graph = defaultdict(list)
-        for v,e in edges:
-            graph[v].append(e)
-            graph[e].append(v)
+        # We can use union find to detect cycle
 
-        visited = set()
-
-        def dfs(node, parent):
-            if node in visited:
-                return False
-            visited.add(node)
-            for neighbor in graph[node]:
-                if neighbor == parent:
-                    continue
-                dfs(neighbor, node)
+        # 1. initilize parents and rank
+        parents = list(range(n))
+        rank = [1] * n
+        # 2. find function, compress the tree
+        def find(x):
+            if parents[x] != x:
+                parents[x] = find(parents[x])
+            return parents[x]
+        # 3. union
+        def union(x,y):
+            rootX = find(x)
+            rootY = find(y)
+            if rootX == rootY:
+                return False # cycle detected!
+            if rank[rootX] < rank[rootY]:
+                parents[rootX] = rootY
+            elif rank[rootY] < rank[rootX]:
+                parents[rootY] = rootX
+            else:
+                parents[rootY] = rootX
+                rank[rootX] += 1
             return True
-        return dfs(0,-1) and len(visited) == n
+        # 4. call the functions
+        for v,e in edges:
+            if not union(v,e):
+                return False
+        return True
+        
