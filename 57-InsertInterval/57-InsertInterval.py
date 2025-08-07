9795315dae4172aeb2f8097cd958c09b3a195938
Nasira Nebi <72615859+nasiranebi914@users.noreply.github.com>
2025-08-07 17:46:09 -0500

    File updated at 8/7/2025, 5:46:09 PM

--- 57-InsertInterval/57-InsertInterval.py
+++ 57-InsertInterval/57-InsertInterval.py
@@ -1,22 +1,15 @@
-# Last updated: 8/7/2025, 4:22:24 PM
+# Last updated: 8/7/2025, 5:46:09 PM
 class Solution:
-    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        # Binasry Search to insert the newInterval
-        i = 0
-        j = len(intervals)-1
-        while i <= j:
-            mid = (i+j)//2
-            if newInterval[0] < intervals[mid][0]:
-                j = mid - 1
-            else:
-                i = mid + 1
-        intervals.insert(i, newInterval)
-        
-        # Merge the overlapped intervals
-        merged = []
+    def removeInterval(self, intervals: List[List[int]], toBeRemoved: List[int]) -> List[List[int]]:
+        ans = []
+
         for i in intervals:
-            if not merged or merged[-1][1] < i[0]:
-                merged.append(i)
+            if i[1] < toBeRemoved[0] or i[0] > toBeRemoved[1]:
+                ans.append(i)
             else:
-                merged[-1][1] = max(merged[-1][1], i[1])
-        return merged
+                if i[0] < toBeRemoved[0]:
+                    ans.append([i[0], toBeRemoved[0]])
+                if toBeRemoved[1] < i[1]:
+                    ans.append([toBeRemoved[1], i[1]])
+        return ans
+            
