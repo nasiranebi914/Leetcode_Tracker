cfc4ab678b2d0e493b27fc1575004b48dfecc126
Nasira Nebi <72615859+nasiranebi914@users.noreply.github.com>
2025-08-11 22:30:11 -0500

    File updated at 8/11/2025, 10:30:11 PM

--- 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.py
+++ 424-LongestRepeatingCharacterReplacement/424-LongestRepeatingCharacterReplacement.py
@@ -1,12 +1,10 @@
-# Last updated: 8/11/2025, 10:27:58 PM
+# Last updated: 8/11/2025, 10:30:11 PM
 class Solution:
+    cache = {0:0, 1:1}
     def fib(self, n: int) -> int:
-        if n <= 1: return n
-        cache = [0] * (n+1)
-        cache[1] = 1
-
-        for i in range(2, n+1):
-            cache[i] = cache[i-1] + cache[i-2]
-
-        return cache[n]
+        if n in self.cache:
+            return self.cache[n]
+        self.cache[n] = self.fib(n-1) + self.fib(n-2)
+        return self.cache[n]
+            
         
