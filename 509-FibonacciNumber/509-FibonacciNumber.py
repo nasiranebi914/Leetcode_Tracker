90b5bc018300d86b2bdf358468393dc5657785ed
Nasira Nebi <72615859+nasiranebi914@users.noreply.github.com>
2025-08-12 15:16:40 -0500

    File updated at 8/12/2025, 3:16:40 PM

--- 509-FibonacciNumber/509-FibonacciNumber.py
+++ 509-FibonacciNumber/509-FibonacciNumber.py
@@ -1,7 +1,9 @@
-# Last updated: 8/12/2025, 3:15:21 PM
+# Last updated: 8/12/2025, 3:16:40 PM
 class Solution:
     cache = {0:0, 1:1, 2:2}
     def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
         for i in range(3, n+1):
             self.cache[i] = self.cache[i-1] + self.cache[i-2]
         return self.cache[n]
