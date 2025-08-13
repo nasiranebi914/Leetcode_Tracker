1b75571118523d15d6bc1d00a535f544953e8801
Nasira Nebi <72615859+nasiranebi914@users.noreply.github.com>
2025-08-13 16:40:27 -0500

    File updated at 8/13/2025, 4:40:27 PM

--- 213-HouseRobberII/213-HouseRobberII.py
+++ 213-HouseRobberII/213-HouseRobberII.py
@@ -1,23 +1,27 @@
-# Last updated: 8/13/2025, 4:20:47 PM
+# Last updated: 8/13/2025, 4:40:27 PM
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1:
+        n = len(nums)
+        if n == 1:
             return nums[0]
-        money1 = 0
-        money2 = 0
-        house1 = 0
-        house2 = 0
-        for i in range(len(nums)-1):
-            money1 = max(nums[i] + house1, house2)
-            house1 = house2
-            house2 = money1
-        
-        house1 = 0
-        house2 = 0
+            
+        dp = [0] * len(nums)
+        dp[-1] = nums[-1]
+        dp[len(nums)-2] = max(nums[n-1],nums[n-2])
 
-        for i in range(1, len(nums)):
-            money2 = max(nums[i] + house1, house2)
-            house1 = house2
-            house2 = money2
+        for i in range(n-3,-1,-1):
+            dp[i] = max(nums[i]+dp[i+2], dp[i+1])
+
+        ans = dp[1]
+
+
+        dp = [0] * len(nums)
+        #dp[-1] = nums[-1]
+        dp[len(nums)-2] = nums[n-2]
+
+        for i in range(n-3,-1,-1):
+            dp[i] = max(nums[i]+dp[i+2], dp[i+1])
+
+        ans = max(ans, dp[0])
+        return ans
         
-        return max(money1, money2)
