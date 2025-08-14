d1189b8ae7a689c530150180176a1b54c98ee58a
Nasira Nebi <72615859+nasiranebi914@users.noreply.github.com>
2025-08-13 21:58:13 -0500

    File updated at 8/13/2025, 9:58:14 PM

--- 567-PermutationinString/567-PermutationinString.py
+++ 567-PermutationinString/567-PermutationinString.py
@@ -1,4 +1,4 @@
-# Last updated: 8/13/2025, 9:57:21 PM
+# Last updated: 8/13/2025, 9:58:14 PM
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         left = 0
@@ -7,7 +7,7 @@ class Solution:
 
         for right in range(len(s)):
             if s[right] in seen and seen[s[right]] >= left:
-                left = seen[s[right]]+1 # if duplicated, move left window to one after duplicated, to start a new window basically
+                left = seen[s[right]] + 1
             seen[s[right]] = right
-            max_len = max(max_len, right-left+1)
+            max_len = max(max_len, right - left + 1)
         return max_len
