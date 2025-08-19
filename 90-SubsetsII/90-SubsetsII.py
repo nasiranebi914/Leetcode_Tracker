d827b994a38492e223a9fc3667197300b94373fa
Nasira Nebi <72615859+nasiranebi914@users.noreply.github.com>
2025-08-19 15:17:42 -0500

    File updated at 8/19/2025, 3:17:42 PM

--- 90-SubsetsII/90-SubsetsII.py
+++ 90-SubsetsII/90-SubsetsII.py
@@ -1,33 +1,28 @@
-# Last updated: 7/8/2025, 9:45:57 PM
+# Last updated: 8/19/2025, 3:17:42 PM
 class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
-        rows = len(board)
-        cols = len(board[0])
-        visited = set()
-        def backtrack(r, c, i):
-            # base case 1: i tracks how many matches we had so far
-            if i == len(word):
-                return True
-            # base case 2: if r and c are out of bounds or invalid chars
-            if (r < 0 or r >= rows or c < 0 or c >= cols or 
-            (r,c) in visited or board[r][c] != word[i]):
-                return False
-            # add visited r and c to visited
-            visited.add((r,c))
-            # explore all the directions
-            res = (
-                backtrack(r+1,c,i+1) or
-                backtrack(r-1,c,i+1) or
-                backtrack(r,c+1,i+1) or
-                backtrack(r,c-1,i+1)
-                )
-            # undo choce
-            visited.remove((r,c))
-            return res
-        for r in range(rows):
-            for c in range(cols):
-                if backtrack(r,c,0):
-                    return True
-        return False
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        res = []
 
+        def backtrack(start, rest, path):
+            # if 0, add to res
+            if rest == 0:
+                res.append(path[:])
+                return
+            # if < 0, means does not add up to target
+            if rest < 0:
+                return
+            
+            for i in range(start, len(candidates)):
+                # choose
+                path.append(candidates[i])
+                # explore
+                # i: add i instead of i+1 to be able to reuse the current number
+                # rest - condidates[i]: because we want to minus the current number everytime
+                backtrack(i, rest - candidates[i], path)
+                # undo
+                path.pop()
+        
+        backtrack(0,target,[])
+        return res
 
+        
