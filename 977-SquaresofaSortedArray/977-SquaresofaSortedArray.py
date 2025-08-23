bf4a1ab04f184689f5223fe7144ebd5bf812b308
Nasira Nebi <72615859+nasiranebi914@users.noreply.github.com>
2025-08-22 21:37:25 -0500

    File updated at 8/22/2025, 9:37:27 PM

--- 977-SquaresofaSortedArray/977-SquaresofaSortedArray.py
+++ 977-SquaresofaSortedArray/977-SquaresofaSortedArray.py
@@ -1,8 +1,20 @@
-# Last updated: 8/22/2025, 8:13:24 PM
+# Last updated: 8/22/2025, 9:37:27 PM
 class Solution:
-    def sortedSquares(self, nums: List[int]) -> List[int]:
-        res = []
-        for i in nums:
-            res.append(i ** 2)
-        res.sort()
-        return res
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        if m == 0:
+            nums1[:] = nums2 
+
+        i = len(nums1)-1
+        j = len(nums2)-1
+        k = m-1
+        while j >= 0:
+            if k >= 0 and nums1[k] > nums2[j]:
+                nums1[i] = nums1[k]
+                k-= 1
+            else:
+                nums1[i] = nums2[j]
+                j -= 1
+            i -= 1
