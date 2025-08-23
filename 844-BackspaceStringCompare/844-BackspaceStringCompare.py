608084f3934cd84b375bf10d2aeb02ccd7896bff
Nasira Nebi <72615859+nasiranebi914@users.noreply.github.com>
2025-08-22 23:07:02 -0500

    File updated at 8/22/2025, 11:07:04 PM

--- 844-BackspaceStringCompare/844-BackspaceStringCompare.py
+++ 844-BackspaceStringCompare/844-BackspaceStringCompare.py
@@ -1,4 +1,4 @@
-# Last updated: 8/22/2025, 10:58:32 PM
+# Last updated: 8/22/2025, 11:07:04 PM
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
         '''
@@ -7,28 +7,15 @@ class Solution:
         ab#c ad#c > ac,ac > true
         ab## c#d# > '','' > true
         '''
-        s1 = []
-        for c in s:
-            '''if c == "#" and s1:
-                s1.pop()
-            else:
+        def build(n):
+            stack = []
+            for c in n:
                 if c != '#':
-                    s1.append(c)'''
-            if c != '#':
-                s1.append(c)
-            else:
-                if s1: 
-                    s1.pop()
-        s2 = []
-        for c in t:
-            '''if c == "#" and s2:
-                s2.pop()
-            else:
-                if c != '#':
-                    s2.append(c)'''
-            if c != '#':
-                s2.append(c)
-            else:
-                if s2:
-                    s2.pop()
-        return s1 == s2
+                    stack.append(c)
+                else:
+                    if stack:
+                        stack.pop()
+            return stack
+        return build(s) == build(t)
+
+        
