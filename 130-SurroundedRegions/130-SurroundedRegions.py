e1c5390ad2cdfe1436bd0c345a197f76c4a50b75
Nasira Nebi <72615859+nasiranebi914@users.noreply.github.com>
2025-10-20 21:48:49 -0500

    File updated at 10/20/2025, 10:48:49 PM

--- 130-SurroundedRegions/130-SurroundedRegions.py
+++ 130-SurroundedRegions/130-SurroundedRegions.py
@@ -1,35 +1,36 @@
-# Last updated: 7/19/2025, 11:38:18 PM
+# Last updated: 10/20/2025, 10:48:49 PM
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
         rows = len(board)
         cols = len(board[0])
 
-        def dfs(r,c):
-            if r < 0 or r >= rows or c < 0 or c >= cols or board[r][c] != 'O':
+        def mark(r,c):
+            if r>=rows or r<0 or c>=cols or c<0 or board[r][c]!='O':
                 return
-            
-            
-            board[r][c] = 'T'
-
+            board[r][c] = 'Z'
             res = (
-                dfs(r+1,c),
-                dfs(r-1,c),
-                dfs(r,c-1),
-                dfs(r,c+1)
+                mark(r+1,c),
+                mark(r-1,c),
+                mark(r,c+1),
+                mark(r,c-1)
             )
-        borders = list(product(range(rows), [0, cols - 1])) + list(
-            product([0, rows - 1], range(cols))
-        )
-
-        for r,c in borders:
-            dfs(r,c)
-
+            return res
+        # mark the borders
+        for c in range(cols):
+            mark(0,c)
+            mark(rows-1,c)
+        for r in range(rows):
+            mark(r,cols-1)
+            mark(r,0)
+        
+        # remark
         for r in range(rows):
             for c in range(cols):
-                if board[r][c] == 'O':
+                if board[r][c] == "O":
                     board[r][c] = 'X'
-                if board[r][c] == 'T':
+                if board[r][c] == 'Z':
                     board[r][c] = 'O'
+
 
         
         
