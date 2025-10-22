ae2e3409d356b372c25289fbae8aaefe8937419f
Nasira Nebi <72615859+nasiranebi914@users.noreply.github.com>
2025-10-22 10:37:26 -0500

    File updated at 10/22/2025, 11:37:25 AM

--- 547-NumberofProvinces/547-NumberofProvinces.py
+++ 547-NumberofProvinces/547-NumberofProvinces.py
@@ -1,16 +1,17 @@
-# Last updated: 10/22/2025, 11:36:03 AM
+# Last updated: 10/22/2025, 11:37:25 AM
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        # build edges that has the connected nodes
+        # build edges from the array isConnected
         edges = []
         n = len(isConnected)
         for i in range(n):
-            for j in range(i+1, n): # to avoid duplicates and self loop
+            for j in range(i+1, n):
                 if isConnected[i][j] == 1:
                     edges.append((i,j))
-        # union find
+        
         parent = list(range(n))
         rank = [1] * n
+
         def find(x):
             if parent[x] != x:
                 parent[x] = find(parent[x])
@@ -20,17 +21,20 @@ class Solution:
             rootY = find(y)
             if rootX == rootY:
                 return False
-            if rank[rootX] < rank[rootY]:
+            if rank[rootX] > rank[rootY]:
+                parent[rootY] = rootX
+            elif rank[rootY] > rank[rootX]:
                 parent[rootX] = rootY
-            elif rank[rootY] < rank[rootX]:
-                parent[rootY] = rootX
             else:
-                parent[rootY] = rootX
+                parent[rootX] = rootY
                 rank[rootX] += 1
             return True
         
-        counter = n
+        count = n
         for v,e in edges:
             if union(v,e):
-                counter -= 1
-        return counter
+                count -= 1
+        return count
+
+            
+
