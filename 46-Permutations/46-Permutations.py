694b337d81247bbcaf88167c37843861330e3d49
Nasira Nebi <72615859+nasiranebi914@users.noreply.github.com>
2025-11-13 16:29:11 -0500

    File updated at 11/13/2025, 4:29:11 PM

--- 46-Permutations/46-Permutations.py
+++ 46-Permutations/46-Permutations.py
@@ -1,30 +1,24 @@
-# Last updated: 11/13/2025, 4:24:42 PM
-class Solution:
-    def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s2) < len(s1): return False
-    
-        counter_a = defaultdict(int)
+# Last updated: 11/13/2025, 4:29:11 PM
+class Solution(object):
+    '''
+    Problem: if s2 has a permutation of s1, return True; else False
+    permutation: same lenght, different order of letters : apple = lepap
+    '''
+    def checkInclusion(self, s1, s2):
+        counter_a = Counter(s1)
         counter_b = defaultdict(int)
+
         left = 0
 
-        for i in s1:
-            counter_a[i] += 1
-        
         for right in range(len(s2)):
             counter_b[s2[right]] += 1
-
-            if right - left + 1 > len(s1):
+            if right-left+1 > len(s1):
                 counter_b[s2[left]] -= 1
                 if counter_b[s2[left]] == 0: del counter_b[s2[left]]
                 left += 1
-            
-            if right - left + 1 == len(s1):
+            if right-left+1 == len(s1):
                 if counter_a == counter_b:
                     return True
-            
         return False
-
-
-
-
+        
         
