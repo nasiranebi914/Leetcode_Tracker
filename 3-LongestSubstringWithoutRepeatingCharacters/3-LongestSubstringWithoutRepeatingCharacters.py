59a712a328981c1047f5924ba79dab7ebd6e4476
Nasira Nebi <72615859+nasiranebi914@users.noreply.github.com>
2025-11-14 16:38:46 -0500

    File updated at 11/14/2025, 4:38:48 PM

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
@@ -1,13 +1,17 @@
-# Last updated: 11/14/2025, 4:38:16 PM
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
+# Last updated: 11/14/2025, 4:38:48 PM
+class Solution(object):
+    # Window Slicing problem
+    def lengthOfLongestSubstring(self, s):
+        seen = {}
         left = 0
         max_len = 0
-        seen = {}
 
         for right in range(len(s)):
             if s[right] in seen and seen[s[right]] >= left:
                 left = seen[s[right]] + 1
             seen[s[right]] = right
-            max_len = max(max_len, right - left + 1)
+            max_len = max(max_len, right-left+1)
         return max_len
+
+
+        
