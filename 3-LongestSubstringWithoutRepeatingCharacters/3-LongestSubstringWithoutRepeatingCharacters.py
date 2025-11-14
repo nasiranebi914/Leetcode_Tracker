36093b007a147d21b6e880cc8ec5a0e7bdade3e6
Nasira Nebi <72615859+nasiranebi914@users.noreply.github.com>
2025-11-14 16:38:15 -0500

    Create 3-LongestSubstringWithoutRepeatingCharacters

---
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
@@ -0,0 +1,13 @@
+# Last updated: 11/14/2025, 4:38:16 PM
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        left = 0
+        max_len = 0
+        seen = {}
+
+        for right in range(len(s)):
+            if s[right] in seen and seen[s[right]] >= left:
+                left = seen[s[right]] + 1
+            seen[s[right]] = right
+            max_len = max(max_len, right - left + 1)
+        return max_len
