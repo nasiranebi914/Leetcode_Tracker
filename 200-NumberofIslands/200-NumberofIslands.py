63764e8955927ebabffc91ac4fb4efbafa0f4ce4
Nasira Nebi <72615859+nasiranebi914@users.noreply.github.com>
2025-11-14 22:15:55 -0500

    File updated at 11/14/2025, 10:15:56 PM

--- 200-NumberofIslands/200-NumberofIslands.py
+++ 200-NumberofIslands/200-NumberofIslands.py
@@ -1,27 +1,26 @@
-# Last updated: 7/12/2025, 9:11:33 PM
+# Last updated: 11/14/2025, 10:15:56 PM
 class Solution(object):
     def numIslands(self, grid):
-        visited = set()
         rows = len(grid)
         cols = len(grid[0])
         counter = 0
 
         def dfs(r,c):
-            if r < 0 or c < 0 or r >= rows or c >= cols or grid[r][c] != '1':
+            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] != "1":
                 return
-            
-            grid[r][c] = '0'
+            grid[r][c] = 0
 
             res = (
-                dfs(r+1,c),
-                dfs(r-1,c),
-                dfs(r,c+1),
-                dfs(r,c-1)
+                dfs(r+1, c),
+                dfs(r-1, c),
+                dfs(r, c+1),
+                dfs(r, c-1)
             )
+            return res
         
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == '1':
-                    dfs(r,c)
+                if grid[r][c] == "1":
                     counter += 1
+                    dfs(r,c)
         return counter
