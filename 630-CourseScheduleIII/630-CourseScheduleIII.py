027163cd1df40730bd0a8754ae23cee5e0d1eac1
Nasira Nebi <72615859+nasiranebi914@users.noreply.github.com>
2025-11-16 13:56:56 -0500

    File updated at 11/16/2025, 1:56:57 PM

--- 630-CourseScheduleIII/630-CourseScheduleIII.py
+++ 630-CourseScheduleIII/630-CourseScheduleIII.py
@@ -1,22 +1,15 @@
-# Last updated: 11/15/2025, 11:43:59 PM
+# Last updated: 11/16/2025, 1:56:57 PM
 class Solution(object):
-    def scheduleCourse(self, courses):
-        courses.sort(key = lambda x : x[1]) # sort by lastDay, cause we want to take lesser deadline ones first
-        max_heap = []
-        days = 0
+    def canAttendMeetings(self, intervals):
+        if not intervals:
+            return True
+        # if there is overlapp, return False
+        intervals.sort(key = lambda x:x[0])
+        prevEnd = intervals[0][1]
 
-        for duration,deadline in courses:
-            # add the duration to max_heap, will keep the longest duration on top
-            heapq.heappush(max_heap, -duration)
-
-            days += duration
-
-            # when days > deadline, we need to remove the longest durated course, to have more space for shorter dura
-            if days > deadline:
-                longest_course = -heapq.heappop(max_heap)
-                days -= longest_course
-        
-        return len(max_heap)
-        
-
-        
+        for i in intervals[1:]:
+            if i[0] >= prevEnd:
+                prevEnd = i[1]
+            else:
+                return False
+        return True
