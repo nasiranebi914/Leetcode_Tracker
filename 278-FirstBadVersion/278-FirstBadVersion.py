f7aeb9e7c448684ab19846e6589cab49e798a503
Nasira Nebi <72615859+nasiranebi914@users.noreply.github.com>
2025-12-01 12:14:26 -0500

    Create 278-FirstBadVersion

---
+++ 278-FirstBadVersion/278-FirstBadVersion.py
@@ -0,0 +1,16 @@
+# Last updated: 12/1/2025, 12:14:26 PM
+1# The isBadVersion API is already defined for you.
+2# def isBadVersion(version: int) -> bool:
+3
+4class Solution:
+5    def firstBadVersion(self, n: int) -> int:
+6        i = 1
+7        j = n
+8
+9        while i < j:
+10            mid = (i+j)//2
+11            if isBadVersion(mid):
+12                j = mid
+13            else:
+14                i = mid+1
+15        return i
