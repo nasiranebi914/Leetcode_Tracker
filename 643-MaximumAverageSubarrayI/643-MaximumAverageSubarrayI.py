c06f781ac900e83d74124c5508a388d7a699097c
Nasira Nebi <72615859+nasiranebi914@users.noreply.github.com>
2025-12-15 11:17:24 -0500

    File updated at 12/15/2025, 11:17:24 AM

--- 643-MaximumAverageSubarrayI/643-MaximumAverageSubarrayI.py
+++ 643-MaximumAverageSubarrayI/643-MaximumAverageSubarrayI.py
@@ -1,18 +1,17 @@
-# Last updated: 8/14/2025, 10:05:37 AM
-class Solution:
-    def characterReplacement(self, s: str, k: int) -> int:
-        left = 0
-        max_window = 0
-        max_freq = 0
-        freq = defaultdict(int)
-
-        for right in range(len(s)):
-            freq[s[right]] += 1
-            max_freq = max(max_freq, freq[s[right]])
-
-            if (right - left + 1) - max_freq > k:
-                freq[s[left]] -= 1
-                left += 1
-            max_window = max(max_window, right - left + 1)
-        return max_window
-
+# Last updated: 12/15/2025, 11:17:24 AM
+1class Solution:
+2    def findMaxAverage(self, nums: List[int], k: int) -> float:
+3        left = 0
+4        max_average = float('-inf')
+5        current_sum = 0
+6
+7        for right in range(len(nums)):
+8            current_sum += nums[right]
+9            if (right - left + 1) == k:
+10                max_average = max(max_average, current_sum / k)
+11                current_sum -= nums[left]
+12                left += 1
+13        return max_average
+14
+15
+16        
